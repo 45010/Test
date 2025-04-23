6cfc48dac5383841b9bcdfe1341c685834fef1fc
45010 <luanpkk@hotmail.com>
2025-04-23 16:47:34 -0300

    Melhorando o Display Ano de Fabricação e Ano de Modelo

---
+++ Teste/DEev-backend-1/DEev-backend-1/Controllers/VeiculosController.cs
@@ -0,0 +1,20 @@
+﻿using DEev_backend_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEev_backend_1.Controllers
+{
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+            return View(dados);
+        }
+    }
+ }
--- Teste/DEev-backend-1/DEev-backend-1/Models/Veiculo.cs
+++ Teste/DEev-backend-1/DEev-backend-1/Models/Veiculo.cs
@@ -13,10 +13,12 @@ namespace DEev_backend_1.Models
         [Required(ErrorMessage = "Obrigátorio informar a Placa do Veículo!")]
         public string Placa { get; set; }
        
-        [Required(ErrorMessage = "Obrigátorio informar o Ano de Fabricação do Veículo!")] 
+        [Required(ErrorMessage = "Obrigátorio informar o Ano de Fabricação do Veículo!")]
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
        
-        [Required(ErrorMessage = "Obrigátorio informar o Ano do Modelo do Veículo!")]   
+        [Required(ErrorMessage = "Obrigátorio informar o Ano do Modelo do Veículo!")]
+        [Display(Name = "Ano do Modelo")]   
         public int AnoModelo { get; set; }
 
     }
